7d7df2c70136aa9761ae05a818ed8a93e6f08195
Matthias Jooß <Jooss.Matthias@gmail.com>
2023-04-06 07:53:04 +0200

    Add files via upload
    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module MMDB
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	// create a new server mux
+	mux := http.NewServeMux()
+	// register the handler function
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World!"))
+	})
+
+	// Test
+	// create a new http server
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	// start the server as a goroutine
+	server.ListenAndServe()
+}
